193615eea50e55ef97d19e34f3e7b2ad5061da24
唐云 <tangyun1992@qq.com>
2023-04-14 11:08:06 +0800

    feat: 权限控制修改

--- src/access.ts
+++ src/access.ts
@@ -8,28 +8,16 @@ export default function access(
   return {
     canAdmin: currentUser?.username === 'super',
     // 商品管理的权限
-    canProduct:
-      permissionList?.find((item) => item.value === 'pms:product:read') ||
-      currentUser?.username === 'super',
+    canProduct: permissionList?.find((item) => item.value === 'pms:product:read'),
     // 品牌列表的权限
-    canBrand:
-      permissionList?.find((item) => item.value === 'pms:brand:read') ||
-      currentUser?.username === 'super',
+    canBrand: permissionList?.find((item) => item.value === 'pms:brand:read'),
     // 新增品牌的权限
-    canBrandCreate:
-      permissionList?.find((item) => item.value === 'pms:brand:create') ||
-      currentUser?.username === 'super',
+    canBrandCreate: permissionList?.find((item) => item.value === 'pms:brand:create'),
     // 更新品牌的权限
-    canBrandUpdate:
-      permissionList?.find((item) => item.value === 'pms:brand:update') ||
-      currentUser?.username === 'super',
+    canBrandUpdate: permissionList?.find((item) => item.value === 'pms:brand:update'),
     // 删除品牌的权限
-    canBrandDelete:
-      permissionList?.find((item) => item.value === 'pms:brand:delete') ||
-      currentUser?.username === 'super',
+    canBrandDelete: permissionList?.find((item) => item.value === 'pms:brand:delete'),
     // 用户列表的权限
-    canUser:
-      permissionList?.find((item) => item.value === 'ums:user:read') ||
-      currentUser?.username === 'super'
+    canUser: permissionList?.find((item) => item.value === 'ums:user:read')
   }
 }
